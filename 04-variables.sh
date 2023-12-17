48e9c96511bc6a94f30e0b304b376609eedd87b4
RAVIRAVEEN <raviraveen7@gmail.com>
2023-12-17 16:29:51 +0530

    edited

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:hai $PERSON2, good morng"
+echo "$PERSON2:hai $PERSON1 very good morng"
+echo "$PERSON1:how are you $PERSON2"
+echo "$PERSON2:iam fine"
