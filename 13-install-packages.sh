0f13234d6ab411c484ca03dd4ee5753d614b9968
RAVIRAVEEN <raviraveen7@gmail.com>
2023-12-20 22:12:33 +0530

    edited

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-R="e\[31m"
-G="e\[32m"
-y="e\[33m"
-N="e\[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
