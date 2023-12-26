2f66914b9f284ea96f3637c375f12e492a575b55
RAVIRAVEEN <raviraveen7@gmail.com>
2023-12-26 14:22:39 +0530

    old log

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -6,8 +6,8 @@ message=""
 
 while IFS= read line 
 do 
-   usage=$(echo $line | awk '{print $1f}' | cut -d % -f1)
-   partition=$(echo $line | awk '{print $1f}')
+   usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+   partition=$(echo $line | awk '{print $1F}')
 
   if [ $usage -ge $DISK_THRESHOLD ]
   then 
